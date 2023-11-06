825f34b02eda6f95388eece26baa4084dafa694f
RuiHong <colorsu1922@163.com>
2023-11-06 09:47:40 +0800

    step35 ok

--- dezero/functions.py
+++ dezero/functions.py
@@ -1,10 +1,6 @@
 import numpy as np
 from dezero.core import Function
 
-
-
-
-
 class Sin(Function):
     def forward(self, x):
         y = np.sin(x)
@@ -28,3 +24,16 @@ class Cos(Function):
 
 def cos(x):
     return Cos()(x)
+
+class Tanh(Function):
+    def forward(self, x):
+        y = np.tanh(x)
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * (1 - y * y)
+        return gx
+
+def tanh(x):
+    return Tanh()(x)
