e1dc0e219d8d882027d3939e5319a9098a4ca321
RuiHong <colorsu1922@163.com>
2023-11-25 10:34:15 +0800

    skipped learning conv, rnn, lstm

---
+++ steps/step54.py
@@ -0,0 +1,18 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import math
+import time
+import numpy as np
+from dezero import test_mode
+import dezero.functions as F
+
+x = np.ones(5)
+print(x)
+
+y = F.dropout(x, dropout_ratio=0.7)
+print(y)
+
+with test_mode():
+    y = F.dropout(x)
+    print(y)
